a9e33edf01f4c6971a9974f07692f3aead37c45c
yoon <kong4170@naver.com>
2023-08-23 21:26:33 +0900

    init

---
+++ ExRIBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExRIBs
+//
+//  Created by 최윤제 on 2023/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
